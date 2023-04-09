773e5508343fc6f4901bcb4a847947be92ce5631
Valgarite <93498358+Valgarite@users.noreply.github.com>
2023-04-09 18:59:50 -0400

    el port
    lol olvidé el port

--- sysprop-backend/src/main.ts
+++ sysprop-backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { PORT } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  
+  await app.listen(PORT);
 }
 bootstrap();
