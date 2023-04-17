6c70359ef70e5fac5f53b955a191b5cd889e3a99
Valgarite <93498358+Valgarite@users.noreply.github.com>
2023-04-17 00:36:41 -0400

    exportar bdd
    autoexplanatorio

--- sysprop-backend/src/backup/backup.controller.ts
+++ sysprop-backend/src/backup/backup.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Res } from '@nestjs/common';
 import * as fs from 'fs';
 import * as util from 'util';
 import { BackupService } from './backup.service';
@@ -11,16 +11,16 @@ export class BackupController {
       
     ){}
 
-    
-    
-
     @Get('export')
-    async export(): Promise<any> {
+    async export(@Res() res): Promise<any> {
       const currentDate = moment().format('DD-MM-YYYY')
 
       const backupDir = `./${currentDate.toLocaleString()}.sql`
       await this.backupService.exportMySQLData(backupDir)
+
+      return res.download(`.\\${backupDir}`)
     }
+
 
     readFile = util.promisify(fs.readFile)
   }
